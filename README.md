635357f418db3290ac5f8dcd6f0840c89ca79d5f
Madda <nobody@nowhere.local>
2015-01-14 23:55:22 +0000

    Make compatible with pip

--- README.md
+++ README.md
@@ -4,5 +4,3 @@ Currently only targeting ansible.
 
 Remaining for 1.0.0:
 * Tests
-* Pipification - include dependency: pyyaml
-* PEP8 checks (especially \_\_init\_\_.py)
---
+++ setup.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+from distutils.core import setup
+import os
+
+setup(
+    name='manage_users',
+    version='1.0.0',
+    description='Utility for managing users via configuration management'
+                ' systems. Currently supports ansible only.',
+    author='geokala',
+    url='https://github.com/geokala',
+    packages=['manage_users'],
+    scripts=[os.path.join('scripts', script)
+             for script in os.listdir('scripts/')],
+    install_requires=['pyyaml'],
+)
